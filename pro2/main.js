ee7fbe6796e9df2e5360814646e8f3feecafcb96
FanciesFancyRats <magriffi1986@gmail.com>
2018-07-02 20:25:58 -0500

    To tired to do anything else today.

---
+++ pro2/main.js
@@ -0,0 +1,20 @@
+story = 1;
+
+function advancePlot(n){
+	if(n === 1){
+		document.getElementById("title").innerHTML = "Inconsiquentiial Survey";
+		alert('Oh no, I think something broke.');	
+
+		story++;
+	}
+	if(n === 2){
+		document.getElementById("description").innerHTML = "This is a short and inconsiquential survey. I'm sorry, I'm so sorry.";
+		story++;
+		document.getElementById("description").innerHTML = "This is a short and inconsiquential survey. I'm sorry, thank you for your time!";
+
+	}
+	if(n === 3){
+		document	
+	}
+
+}
